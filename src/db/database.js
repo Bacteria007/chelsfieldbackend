912513905913fdc315e85a8257c4f697328356ba
Aqsa Hamayun <123715803+Bacteria007@users.noreply.github.com>
2024-11-29 10:42:59 +0500

    slider backend

--- src/db/database.js
+++ src/db/database.js
@@ -1,8 +1,13 @@
-import mongoose from "mongoose";
-// export const connectDB = async()=>{
-//   await  mongoose.connect(`${process.env.MONGODB_URL}`,{
-//         dbName: "Chelsefield"
-//     })
-//     .then((c)=> console.log(`MongoDB Connected to ${c.connection.host}`))
-//     .catch((e)=> console.log(e))
-// }
+const mongoose = require('mongoose');
+
+exports.connectDB = async () => {
+  try {
+    const connection = await mongoose.connect(process.env.MONGODB_URI, {
+      dbName: "Chelsefield", // Specify the database name here
+    });
+    console.log(`MongoDB connected: ${connection.connection.host}`);
+  } catch (error) {
+    console.error(`Error connecting to MongoDB: ${error.message}`);
+    process.exit(1); // Exit process with failure
+  }
+};
